7934f2440bf918d7e82da60b136f4c2b96419218
Aki <s.aki.w@proton.me>
2025-02-20 23:58:06 +0900

    Finished Debug Section of VSCode Django tutorial

--- Games_List/urls.py
+++ Games_List/urls.py
@@ -3,4 +3,5 @@ from Games_List import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("Games_List/<name>", views.test_code, name="hello_there"),
 ]
--- Games_List/views.py
+++ Games_List/views.py
@@ -1,5 +1,20 @@
+import re
+import django.utils.timezone; import datetime
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
 
+def test_code(request, name):
+    now = datetime.datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y at %x")
+    
+    match_object = re.match("[a-zA-Z]+", name)
+    
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+        
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
